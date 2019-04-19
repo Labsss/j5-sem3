959983742bbf675a3220ebc7fd6f1fcf48b9cf7b
Labsss <alexandre.labonne@gmail.com>
2019-04-19 16:26:57 +0200

    3rd step

---
+++ lib/gossip.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Gossip
+    attr_reader :author, :content
+
+    def initialize(author, content)
+        @content = content
+        @author = author
+    end
+
+binding.pry
+    
+    def save
+        *****************************************
+    end
